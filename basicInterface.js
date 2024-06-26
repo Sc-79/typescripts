0516c176bbd6a8a2af1d8478daf6333457979087
sourav <souravchongrey@gmail.com>
2024-06-26 16:19:04 +0530

    basic inter face

---
+++ basicInterface.js
@@ -0,0 +1,6 @@
+var persion = {
+    name: "sourav chongrey",
+    age: 30,
+};
+console.log(persion.age);
+console.log(persion.name);
---
+++ basicInterface.ts
@@ -0,0 +1,11 @@
+interface Persion{
+    name:string;
+    age:number;
+}
+
+const persion:Persion ={
+    name:"sourav chongrey",
+    age:30,
+};
+console.log(persion.age);
+console.log(persion.name);
