996f2c58422d45244bf99480c9d45b5bfbb3170c
sourav <souravchongrey@gmail.com>
2024-06-26 20:49:00 +0530

    add to union type

---
+++ UnionType.js
@@ -0,0 +1,5 @@
+function printId(id) {
+    console.log('id:', id);
+}
+printId('1234');
+printId(1234);
---
+++ UnionType.ts
@@ -0,0 +1,7 @@
+function printId (id: string | number){
+    console.log('id:',id);
+
+}
+
+printId('1234');
+printId(1234);
